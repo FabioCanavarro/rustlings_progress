d017288135cadf1217a7c84d437e5f8792dcc5bb
Fabio Canavarro <fabiocanavarrotoh@gmail.com>
2024-12-12 22:07:45 +0700

    iterator and test

--- exercises/18_iterators/iterators2.rs
+++ exercises/18_iterators/iterators2.rs
@@ -1,32 +1,56 @@
 // In this exercise, you'll learn some of the unique advantages that iterators
 // can offer.
 
+use std::{vec};
+
 // TODO: Complete the `capitalize_first` function.
 // "hello" -> "Hello"
-fn capitalize_first(input: &str) -> String {
+pub fn capitalize_first(input: &str) -> String {
     let mut chars = input.chars();
-    match chars.next() {
+    let mut x: String = match chars.next() {
         None => String::new(),
-        Some(first) => todo!(),
+        Some(first) => first.to_string().to_uppercase(),
+    };
+    if x.is_empty(){
+        x
     }
+    else {
+        for i in chars{
+            x.push(i);
+        }
+         x
+    }
+
+
 }
 
 // TODO: Apply the `capitalize_first` function to a slice of string slices.
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
-fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    // ???
+pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
+    let mut x: Vec<String> = vec![];
+    for i in words.iter(){
+        x.push(capitalize_first(i));
+    }
+    x
 }
 
 // TODO: Apply the `capitalize_first` function again to a slice of string
 // slices. Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
-fn capitalize_words_string(words: &[&str]) -> String {
-    // ???
+pub fn capitalize_words_string(words: &[&str]) -> String {
+    let mut y: String = Default::default();
+    let x = capitalize_words_vector(words);
+    for i in x.iter(){
+        y.push_str(i);
+    }
+     y
 }
 
 fn main() {
     // You can optionally experiment here.
+    let x = "hahah";
+    print!("{}",capitalize_first(x));
 }
 
 #[cfg(test)]
